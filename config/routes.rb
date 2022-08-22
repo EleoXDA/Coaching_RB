f48d0b97e9e94e601afd08b2dc4c9d29b4a5e5ee
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-22 21:21:28 +0200

    FIX: system working no CSS

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'ask', to: 'questions#ask'
-  get 'answer', to: 'questions#answer'
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
 end
