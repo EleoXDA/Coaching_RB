f92a7ade67174fa1cb16a307b0d490ba8a414423
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-22 20:36:54 +0200

    ADD: Q-A with coach

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,15 @@
 class QuestionsController < ApplicationController
-    def ask
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.include? '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
     end
+  end
 end
