797f9391ea76bf15ee061395f902a33e2e203bf8
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-22 21:13:42 +0200

    TRY: alternative fix

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,7 +6,7 @@ class QuestionsController < ApplicationController
     @question = params[:question]
     if @question == 'I am going to work'
       @answer = 'Great!'
-    elsif @question.include? '?'
+    elsif @question[-1] == '?'
       @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = 'I don\'t care, get dressed and go to work!'
