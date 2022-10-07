e335b9a371f20cc5fd1c3b6fac96eb4fbf8703e5
Elbay Malikmammadov <eleo.666@gmail.com>
2022-10-07 23:20:21 +0200

    fix empty index screen

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "questions#ask"
   get 'ask', to: 'questions#ask', as: :ask
   get 'answer', to: 'questions#answer', as: :answer
 end
