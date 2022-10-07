6469c7188d65026fc53995ca117dee59ee93547e
Elbay Malikmammadov <eleo.666@gmail.com>
2022-10-07 23:09:11 +0200

    add blank question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,12 +4,14 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-    if @question == 'I am going to work'
-      @answer = 'Great!'
-    elsif @question[-1] == '?'
-      @answer = 'Silly question, get dressed and go to work!'
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
+      @answer = "Great!"
+    elsif @question.ends_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
     else
-      @answer = 'I don\'t care, get dressed and go to work!'
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
